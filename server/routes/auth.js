019d8f3a4b96c37b05d6aaa868333ba9b1ac66e4
tharunsi Name <tharunsitharunsi@gmail.com>
2025-07-01 09:39:06 +0530

    last

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import User from '../models/User.js';
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs';
+
 import jwt from 'jsonwebtoken'
 import middleware from '../middleware/middleware.js';
 import dotenv from "dotenv";
