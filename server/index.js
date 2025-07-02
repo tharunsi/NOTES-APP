8cfd18f726d455f794e650b4f4e727d76ba31f5b
tharunsi Name <tharunsitharunsi@gmail.com>
2025-07-02 15:44:27 +0530

    last

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv';
 
 
 import MongoDB from './db/db.js'
 import authRouter from './routes/auth.js'
 import noteRouter from './routes/note.js'
+
+dotenv.config()
 const app = express()
-app.use(cors())
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://notes-app-9y7d-tau.vercel.app' 
+];
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json())
 app.use('/api/auth',authRouter)
 app.use('/api/note',noteRouter)
 
-app.listen(5000, () => {
-    MongoDB()
-    console.log("server is running")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  MongoDB(); 
+  console.log(`Server running on port ${PORT}`);
+});
